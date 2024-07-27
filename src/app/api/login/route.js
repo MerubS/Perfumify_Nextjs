3fa23aac5c1ad6b3c99b804bc6841a3b2039950c
MerubS <smerub@yahoo.com>
2024-07-27 22:07:23 +0500

    updated

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -4,11 +4,18 @@ import path from 'path';
 import fs from 'fs';
 import { generateToken } from '@/app/utils/jwt';
 
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*', // Adjust this to your domain in production
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
+
+
 export async function POST(req) {
   try {
     const { email, password } = await req.json();
     if (!email || !password) {
-      return NextResponse.json({ error: 'Email and password cannot be empty.' }, { status: 400 });
+      return NextResponse.json({ error: 'Email and password cannot be empty.' }, { status: 400 , headers: { ...corsHeaders, 'Content-Type': 'application/json' } });
     }
 
     const filePath = path.join(process.cwd(), 'public', 'userCredentials.json');
@@ -17,20 +24,20 @@ export async function POST(req) {
     const user = users.find(user => user.email === email);
 
     if (!user) {
-      return NextResponse.json({ error: 'User not found.' }, { status: 404 });
+      return NextResponse.json({ error: 'User not found.' }, { status: 404 ,headers: { ...corsHeaders, 'Content-Type': 'application/json' }});
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (isMatch) {
       const token = generateToken({ email: user.email });
-      return NextResponse.json({ message: 'Login successful.', token }, { status: 200 });
+      return NextResponse.json({ message: 'Login successful.', token }, { status: 200 , headers: { ...corsHeaders, 'Content-Type': 'application/json' } });
     } else {
-      return NextResponse.json({ error: 'Invalid credentials.' }, { status: 401 });
+      return NextResponse.json({ error: 'Invalid credentials.' }, { status: 401, headers: { ...corsHeaders, 'Content-Type': 'application/json' } });
     }
   } catch (err) {
     console.error('Error during login:', err);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 , headers: { ...corsHeaders, 'Content-Type': 'application/json' } });
   }
 }
 
